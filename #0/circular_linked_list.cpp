38d82136363477b07d82c917e9823e66e2ff991c
ady2008ad <ady2008ad@gmail.com>
2025-08-18 22:15:26 +0530

    18 aug

--- #0/circular_linked_list.cpp
+++ #0/circular_linked_list.cpp
@@ -1,61 +1,74 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
 
-    Node(int d){
-        data=d;
-        next=NULL;
+    Node(int d)
+    {
+        data = d;
+        next = NULL;
     }
 };
 
-class CircularLL{
-    public:
-    Node *head=NULL;
+class CircularLL
+{
+public:
+    Node *head = NULL;
 
-    void insert_at_beg(int d){
-        Node *newNode=new Node(d);
-        if(head==NULL){
-            head=newNode;
-            newNode->next=newNode;
+    void insert_at_beg(int d)
+    {
+        Node *newNode = new Node(d);
+        if (head == NULL)
+        {
+            head = newNode;
+            newNode->next = newNode;
         }
-        else{
-            Node *temp=head;
-            while(temp->next!=head){
-                temp=temp->next;
+        else
+        {
+            Node *temp = head;
+            while (temp->next != head)
+            {
+                temp = temp->next;
             }
-            temp->next=newNode;
-            newNode->next=head;
-            head=newNode;
+            temp->next = newNode;
+            newNode->next = head;
+            head = newNode;
         }
     }
 
-    void insert_at_end(int d){
-        Node *newNode=new Node(d);
-        if(head==NULL){
-            head=newNode;
-            newNode->next=newNode;
+    void insert_at_end(int d)
+    {
+        Node *newNode = new Node(d);
+        if (head == NULL)
+        {
+            head = newNode;
+            newNode->next = newNode;
         }
-        else{
-            Node *temp=head;
-            while(temp->next!=head){
-                temp=temp->next;
+        else
+        {
+            Node *temp = head;
+            while (temp->next != head)
+            {
+                temp = temp->next;
             }
-            temp->next=newNode;
-            newNode->next=head;
+            temp->next = newNode;
+            newNode->next = head;
         }
     }
 
-    void insert_at_pos(int pos,int d){
-        Node *newNode=new Node(d);
-        Node *temp=head;
-        for(int i=0;i<pos-1;i++){
-            temp=temp->next;
+    void insert_at_pos(int pos, int d)
+    {
+        Node *newNode = new Node(d);
+        Node *temp = head;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            temp = temp->next;
         }
-        newNode->next=temp->next;
-        temp->next=newNode;
+        newNode->next = temp->next;
+        temp->next = newNode;
     }
 };
