73f3ba43853bcf0e51c8932fd216793e77ea2db5
ady2008ad <ady2008ad@gmail.com>
2025-10-05 19:23:30 +0530

    5 Oct

--- Experiments_Java/Experiment_8.1.java
+++ Experiments_Java/Experiment_8.1.java
@@ -1,23 +1,14 @@
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        
-        int num1 = sc.nextInt();
-
-        int num2 = sc.nextInt();
-
-        int a = num1;
-        int b = num2;
-
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(i%2==0 || i%3==0){
+                sum=sum+i;
+            }
         }
-
-        System.out.println("The GCD of " + num1 + " and " + num2 + " is: " + a);
+        System.out.println("Sum of numbers divisible by 2 or 3: "+sum);
     }
 }
--- Experiments_Java/Experiment_9.java
+++ Experiments_Java/Experiment_9.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
-        System.out.print("All the divisors of n: ");
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                System.out.print(i+" ");
-            }
+        int r=1;
+        int a=n;
+        while(n>0){
+            r=r*n;
+            n--;
         }
+        System.out.println("Factorial of "+a+" is "+r);
     }
 }
