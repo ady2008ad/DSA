1e6b69baa42534a547e89d8a55d425497f7bb9d9
ady2008ad <ady2008ad@gmail.com>
2025-10-13 09:13:38 +0530

    13 Oct

---
+++ Experiments/Eperiment_9.cpp
@@ -0,0 +1,134 @@
+// Binary Search Tree(BST): It is a special type of binary tree that organize data so that searching, insertion, deletion can be done in a organized manner.
+// Properties:
+//   1.All keys in left subtree of a node are smaller than that node key.
+//   2.All keys in the right subtree of a node are larger than that node key.
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int key;
+    Node *left;
+    Node *right;
+
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+class BST
+{
+    Node *root;
+
+    Node *insert_rec(Node *root, int k)
+    {
+        if (root == NULL)
+        {
+            return new Node(k);
+        }
+        if (k < root->key)
+        {
+            root->left = insert_rec(root->left, k);
+        }
+        else
+        {
+            root->right = insert_rec(root->right, k);
+        }
+        return root;
+    }
+
+    int minValue(Node *root)
+    {
+        int minv = root->key;
+        while (root->left != NULL)
+        {
+            minv = root->left->key;
+            root = root->left;
+        }
+        return minv;
+    }
+
+    Node *del_rec(Node *root, int key)
+    {
+        if (root == NULL)
+        {
+            return root;
+        }
+
+        if (key < root->key)
+        {
+            root->left = del_rec(root->left, key);
+        }
+        else if (key > root->key)
+        {
+            root->right = del_rec(root->right, key);
+        }
+        else
+        {
+            if (root->left == NULL)
+                return root->right;
+            else if (root->right == NULL)
+                return root->left;
+
+            root->key = minValue(root->right);
+
+            root->right = del_rec(root->right, root->key);
+        }
+
+        return root;
+    }
+
+public:
+    BST()
+    {
+        root = NULL;
+    }
+
+    void insert(int k)
+    {
+        root = insert_rec(root, k);
+    }
+
+    void del(int key)
+    {
+        root = del_rec(root, key);
+    }
+
+    void inorder(Node *root)
+    {
+        if (root != NULL)
+        {
+            inorder(root->left);
+            cout << root->key << " ";
+            inorder(root->right);
+        }
+    }
+
+    void display()
+    {
+        inorder(root);
+        cout << endl;
+    }
+};
+
+int main()
+{
+    BST obj;
+    obj.insert(5);
+    obj.insert(3);
+    obj.insert(7);
+    obj.insert(10);
+
+    obj.display();
+    return 0;
+}
+/*
+      5
+     / \
+    3   7
+         \
+          10
+*/
---
+++ Experiments/Experiment_7.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d)
+    {
+        data = d;
+        left = right = NULL;
+    }
+    void preorder(Node *root)
+    { // Root->Left->Right
+        if (root == NULL)
+        {
+            return;
+        }
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+    void inorder(Node *root)
+    { // Left->Root->Right
+        if (root == NULL)
+        {
+            return;
+        }
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+    void postorder(Node *root)
+    { // Left->Right->Root
+        if (root == NULL)
+        {
+            return;
+        }
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+    void deletDeepest(Node *root, Node *dNode)
+    {
+        queue<Node *> q;
+        q.push(root);
+
+        Node *curr;
+        while (!q.empty())
+        {
+            curr = q.front();
+            q.pop();
+
+            if (curr == dNode)
+            {
+                curr = nullptr;
+                delete (dNode);
+                return;
+            }
+
+            if (curr->right)
+            {
+
+                if (curr->right == dNode)
+                {
+                    curr->right = nullptr;
+                    delete (dNode);
+                    return;
+                }
+                q.push(curr->right);
+            }
+
+            if (curr->left)
+            {
+
+                if (curr->left == dNode)
+                {
+                    curr->left = nullptr;
+                    delete (dNode);
+                    return;
+                }
+                q.push(curr->left);
+            }
+        }
+    }
+
+    Node *deletion(Node *root, int key)
+    {
+
+        if (root == nullptr)
+            return nullptr;
+
+        if (root->left == nullptr && root->right == nullptr)
+        {
+
+            if (root->data == key)
+                return nullptr;
+            else
+                return root;
+        }
+
+        queue<Node *> q;
+        q.push(root);
+
+        Node *curr;
+        Node *keyNode = nullptr;
+
+        while (!q.empty())
+        {
+            curr = q.front();
+            q.pop();
+
+            if (curr->data == key)
+                keyNode = curr;
+
+            if (curr->left)
+                q.push(curr->left);
+
+            if (curr->right)
+                q.push(curr->right);
+        }
+
+        if (keyNode != nullptr)
+        {
+
+            int x = curr->data;
+
+            keyNode->data = x;
+
+            deletDeepest(root, curr);
+        }
+        return root;
+    }
+};
+int main()
+{
+    Node *root = new Node(5);
+    root->left = new Node(3);
+    root->right = new Node(7);
+    root->left->left = new Node(10);
+    cout << "Inorder traversing: ";
+    root->inorder(root);
+    cout << endl;
+    cout << "Preorder traversing: ";
+    root->preorder(root);
+    cout << endl;
+    cout << "Postorder traversing: ";
+    root->postorder(root);
+    cout << endl;
+    root->deletion(root,3);
+    cout << "Postorder traversing: ";
+    root->postorder(root);
+    cout<<endl;
+    return 0;
+}
+/*
+        5
+       / \
+      3   7
+     /
+    10
+*/
