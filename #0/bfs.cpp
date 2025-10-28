77e4fdae9283adcb2866b199498ee7477ba2a377
ady2008ad <ady2008ad@gmail.com>
2025-10-28 21:34:39 +0530

    28 Oct

---
+++ #0/bfs.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class GraphRep {
+public:
+    int v;
+    int **mat;
+
+    GraphRep(int v1) {
+        v = v1;
+        mat = new int*[v];
+        for (int i = 0; i < v; i++) {
+            mat[i] = new int[v];
+            for (int j = 0; j < v; j++)
+                mat[i][j] = 0;
+        }
+    }
+
+    void addEdge(int s, int d) {
+        mat[s-1][d-1] = 1;
+        mat[d-1][s-1] = 1; 
+    }
+
+    void display() {
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                cout << mat[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    void bfs(int s,int max){
+        s=s-1;
+        int *visited=new int[max]();
+        int *q=new int[max];
+        int front,rear;
+        front=rear=-1;
+        visited[s]=1;
+        q[++rear]=s;
+        front=0;
+        cout<<"BFS:"<<endl;
+        while(front<=rear){
+            int current=q[front];
+            front++;
+            cout<<current+1<<" ";
+            for(int i=0;i<v;i++){
+                if(mat[current][i]==1 && visited[i]!=1){
+                    visited[i]=1;
+                    q[++rear]=i;
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    int v, e;
+    cout << "Enter number of vertices: ";
+    cin >> v;
+    cout << "Enter number of edges: ";
+    cin >> e;
+    GraphRep obj(v);
+
+    for (int i = 0; i < e; i++) {
+        int s, d;
+        cout << "Enter source and destination: ";
+        cin >> s >> d;
+        obj.addEdge(s, d);
+    }
+
+    obj.display();
+    obj.bfs(1,v);
+    return 0;
+}
---
+++ #0/dfs.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+class GraphRep {
+public:
+    int v;
+    int **mat;
+
+    GraphRep(int v1) {
+        v = v1;
+        mat = new int*[v];
+        for (int i = 0; i < v; i++) {
+            mat[i] = new int[v];
+            for (int j = 0; j < v; j++)
+                mat[i][j] = 0;
+        }
+    }
+
+    void addEdge(int s, int d) {
+        mat[s-1][d-1] = 1;
+        mat[d-1][s-1] = 1; // undirected
+    }
+
+    void display() {
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                cout << mat[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    void dfs(int s,int max){
+        s=s-1;
+        int *visited=new int[max]();
+        int *st=new int[max];
+        int top=-1;
+        visited[s]=1;
+        st[++top]=s;
+        cout<<"DFS:"<<endl;
+        while(top!=-1){
+            int current=st[top--];
+            cout<<current+1<<" ";
+            for(int i=0;i<v;i++){
+                if(mat[current][i]==1 && visited[i]!=1){
+                    visited[i]=1;
+                    st[++top]=i;
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    int v, e;
+    cout << "Enter number of vertices: ";
+    cin >> v;
+    cout << "Enter number of edges: ";
+    cin >> e;
+    GraphRep obj(v);
+
+    for (int i = 0; i < e; i++) {
+        int s, d;
+        cout << "Enter source and destination: ";
+        cin >> s >> d;
+        obj.addEdge(s, d);
+    }
+
+    obj.display();
+    obj.dfs(1,v);
+    return 0;
+}
